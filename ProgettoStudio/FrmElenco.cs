0a4778a1353809541e0f8d04fc026c2ba1ea6ec5
Edoardo RADAELLI <e.radaelli@edisoftware.it>
2025-05-19 17:21:48 +0200

    prima commit

---
+++ ProgettoStudio/FrmElenco.cs
@@ -0,0 +1,75 @@
+﻿using Entity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgettoStudio
+{
+    public partial class FrmElenco : Form
+    {
+
+        Func<IEnumerable> RefreshDelegate { get; set; }
+
+        Action<EntityBase> EditDelegate { get; set; }
+
+        public FrmElenco()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            if (RefreshDelegate != null)
+            {
+                var elenco = RefreshDelegate.Invoke();
+
+                dataGridView.DataSource = elenco.Cast<object>().ToList();
+            }
+
+            dataGridView.CellDoubleClick += DataGridView_CellDoubleClick;
+
+        }
+
+        private void NewButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DataGridView_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var grid = sender as DataGridView;
+
+            if (grid != null && e.RowIndex >= 0)
+            {
+                var row = grid.Rows[e.RowIndex];
+                var item = row.DataBoundItem;
+
+            }
+
+
+            this.ShowDialog();
+
+        }
+
+
+
+
+        private void ShowModal()
+        { 
+            
+        
+        
+        
+        }
+
+    }
+}
